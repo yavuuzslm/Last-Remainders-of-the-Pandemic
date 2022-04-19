37e53b09ac62e212025af7de36cedd9e593c2cfc
Deniz Aydemir <d.aydemir@ug.bilkent.edu.tr>
2022-04-19 23:10:41 +0300

    I added TMX configuration

---
+++ core/src/com/lastremaindersofthepandemic/com/Entity.java
@@ -0,0 +1,28 @@
+package com.lastremaindersofthepandemic.com;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+
+public class Entity {
+    public Vector2 pos;
+    public Vector3 pos3;
+    public Texture texture;
+    public float width;
+    public float height;
+    public float speed;
+
+    float dirX = 0;
+    float dirY = 0;
+
+    public Entity() {
+        pos = new Vector2();
+        pos3 = new Vector3();
+    }
+
+    public void draw(SpriteBatch batch) {
+        batch.draw(texture, pos.x, pos.y, width, height);
+    }
+
+}
--- core/src/com/lastremaindersofthepandemic/com/GameWorld.java
+++ core/src/com/lastremaindersofthepandemic/com/GameWorld.java
@@ -6,16 +6,24 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class GameWorld extends ApplicationAdapter {
     SpriteBatch batch;
     Texture img;
     OrthographicCamera camera;
     Control control;
+    TiledMap tiledMap;
+    TiledMapRenderer tiledMapRenderer;
 
     //Display size
     private int displayW;
     private int displayH;
+
 
     //Temp x and y co-ords
     int x, y;
@@ -29,16 +37,15 @@ public class GameWorld extends ApplicationAdapter {
     @Override
     public void create () {
         batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
 
-        displayW = Gdx.graphics.getWidth();
-        displayH = Gdx.graphics.getHeight();
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
 
-        int h = (int) (displayH/Math.floor(displayH / 160));
-        int w = (int) (displayW/(displayH / (displayH / Math.floor(displayW / 160))));
-
-        camera = new OrthographicCamera(w, h);
-        camera.zoom = .4f;
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false,w,h);
+        camera.update();
+        tiledMap = new TmxMapLoader().load("harita1.tmx");
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
 
         control = new Control(displayW, displayH, camera);
         Gdx.input.setInputProcessor(control);
@@ -46,7 +53,7 @@ public class GameWorld extends ApplicationAdapter {
 
     @Override
     public void render () {
-        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClearColor(0,0,1,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         //reset the direction values
@@ -72,13 +79,17 @@ public class GameWorld extends ApplicationAdapter {
         camera.update();
 
         //game draw
+        batch.begin();
         batch.setProjectionMatrix(camera.combined);
         batch.setBlendFunction(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 
+        //new
+        tiledMapRenderer.setView(camera);
+        tiledMapRenderer.render();
 
         //ScreenUtils.clear(1, 0, 0, 1);
-        batch.begin();
-        batch.draw(img, 0, 0);
+
+        //batch.draw(img, 0, 0);
         batch.end();
     }
 
